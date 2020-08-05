7afdf1a5d4321ae8ba04e06d46e818a6d3c8e764
Luca Stefani <luca.stefani.ge1@gmail.com>
2020-08-05 19:51:51 +0200

    goodix: Enable touch in recovery
    Change-Id: I0f0d116463c916c9eba39fb9a756d324b488dd5c

--- drivers/input/touchscreen/goodix_gtx8/goodix_ts_core.c
+++ drivers/input/touchscreen/goodix_gtx8/goodix_ts_core.c
@@ -33,10 +33,6 @@
 #define PINCTRL_STATE_ACTIVE    "pmx_ts_active"
 #define PINCTRL_STATE_SUSPEND   "pmx_ts_suspend"
 
-#ifdef ASUS_ZS661KS_PROJECT
-extern bool g_Recovery_mode;
-#endif
-
 static int goodix_ts_remove(struct platform_device *pdev);
 int goodix_start_later_init(struct goodix_ts_core *ts_core);
 void goodix_ts_dev_release(void);
@@ -2179,13 +2175,6 @@ static void goodix_resume_work(struct work_struct *work)
 }
 
 void phone_touch_resume(void) {
-
-#ifdef ASUS_ZS661KS_PROJECT
-	if(g_Recovery_mode) {
-		ts_info("phone_touch_resume(): not for recovery mode");
-		return ;
-	}
-#endif
 	/* driver probe failed */
 	if (!goodix_modules.core_data ||
 	    !goodix_modules.core_data->initialized) {
@@ -2211,14 +2200,6 @@ void phone_touch_resume(void) {
 EXPORT_SYMBOL_GPL(phone_touch_resume);
 
 void phone_touch_suspend(void) {
-
-#ifdef ASUS_ZS661KS_PROJECT
-	if(g_Recovery_mode) {
-		ts_info("phone_touch_suspend(): not for recovery mode");
-		return ;
-	}
-#endif
-
 	/* driver probe failed */
 	if (!goodix_modules.core_data ||
 	    !goodix_modules.core_data->initialized) {
@@ -3869,12 +3850,6 @@ static struct platform_driver goodix_ts_driver = {
 
 int goodix_ts_core_init(void)
 {
-#ifdef ASUS_ZS661KS_PROJECT
-	if(g_Recovery_mode) {
-		ts_info("Core layer init: not for recovery mode");
-		return 0;
-	}
-#endif
 	ts_info("Core layer init");
 	if (!goodix_modules.initilized) {
 		/* this may init by outer modules register event */
